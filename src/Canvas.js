175e07b1a1bc9e6758eb350c719ac2d7e14bc237
PrimoPan <898021802@qq.com>
2023-03-27 15:17:31 +0800

    update

---
+++ src/Canvas.js
@@ -0,0 +1,71 @@
+import React, { useRef, useEffect } from 'react';
+var Deque = require("collections/deque");
+
+const Canvas = () => {
+
+
+    var client = null,
+        screenW = null,
+        screenH = null,
+        time = null,
+        canvas = null,
+        context = null,
+        objSize = 50;
+
+
+    const canvasRef = useRef(null);
+    var deque=new Deque();
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        let mouseX = 0;
+        let mouseY = 0;
+        let lastMouseX = 0;
+        let lastMouseY = 0;
+        let mouseSpeed = 0;
+
+        canvas.width = 1024;
+        canvas.height = 768;
+
+        const updateMousePosition = (event) => {
+            lastMouseX = mouseX;
+            lastMouseY = mouseY;
+            mouseX = event.clientX - canvas.offsetLeft;
+            mouseY = event.clientY - canvas.offsetTop;
+            mouseSpeed = Math.sqrt(Math.pow(mouseX - lastMouseX, 2) + Math.pow(mouseY - lastMouseY, 2));
+            let obj=new Object();
+            obj.posX=mouseX;
+            obj.posY=mouseY;
+            obj.speed=mouseSpeed;
+            if (deque.length>=50)
+                deque.shift();
+            deque.push(obj);
+        };
+
+        const draw = () => {
+            let obj=deque.peekBack();
+            if (obj) {
+                ctx.fillStyle = "#009fe3";
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.fillText(`Mouse position: (${obj.posX}, ${obj.posY})`, 10, 20);
+                ctx.fillText(`Mouse speed: ${obj.speed.toFixed(2)}`, 10, 40);
+
+            }
+            requestAnimationFrame(draw);
+        };
+
+        canvas.addEventListener('mousemove', updateMousePosition);
+        draw();
+
+        return () => {
+            canvas.removeEventListener('mousemove', updateMousePosition);
+        };
+    }, []);
+    const debug= event =>{
+        console.log(deque.length)
+        console.log(deque)
+    }
+    return <canvas tabIndex={0} ref={canvasRef} onKeyDown={debug}/>;
+};
+
+export default Canvas;
